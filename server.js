6694f8fb89ee083fe9d5e9e8d9ac7b7984bedb97
sk <kinarsardhara225@gmail.com>
2024-12-04 22:02:10 +0530

    commit

--- server.js
+++ server.js
@@ -1,48 +1,53 @@
-import express from "express"
-import dotenv from "dotenv"
-import path from "path"
-import authRoutes from "./routes/auth.routes.js"
-import userRoutes from "./routes/user.routes.js"
-import postRoutes from "./routes/post.routes.js"
-import searchRoutes from "./routes/search.routes.js"
-import notificationRoutes from "./routes/notification.routes.js"
-import connectMongoDB from "./db/connectMongoDB.js"
-import cookieParser from "cookie-parser"
-import { v2 as cloudinary } from "cloudinary"
+import express from "express";
+import dotenv from "dotenv";
+import path from "path";
+import authRoutes from "./routes/auth.routes.js";
+import userRoutes from "./routes/user.routes.js";
+import postRoutes from "./routes/post.routes.js";
+import searchRoutes from "./routes/search.routes.js";
+import notificationRoutes from "./routes/notification.routes.js";
+import connectMongoDB from "./db/connectMongoDB.js";
+import cookieParser from "cookie-parser";
+import { v2 as cloudinary } from "cloudinary";
+import cors from "cors";
+dotenv.config({ path: "./config.env" });
 
-dotenv.config({path:"./config.env"});
-
-const app = express()
-const __dirname = path.resolve()
-
+const app = express();
+const __dirname = path.resolve();
 
 cloudinary.config({
-    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-    api_key: process.env.CLOUDINARY_API_KEY,
-    api_secret: process.env.CLOUDINARY_API_SECRET
-})
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
 
-const PORT = process.env.PORT || 8000
+const PORT = process.env.PORT || 8000;
 
-app.use(express.json({ limit: "5mb" })) // it is a middleware for parsing req.body
-app.use(express.urlencoded({ extended: true })) // it is a middleware to parse form data
-app.use(cookieParser())
+app.use(express.json({ limit: "5mb" })); // it is a middleware for parsing req.body
+app.use(express.urlencoded({ extended: true })); // it is a middleware to parse form data
+app.use(cookieParser());
 
-app.use("/api/auth", authRoutes)
-app.use("/api/users", userRoutes)
-app.use("/api/posts", postRoutes)
-app.use("/api/notifications", notificationRoutes)
-app.use("/api/search", searchRoutes)
+const corsOptions = {
+  origin: process.env.FRONT_URL,
+  credentials: true
+};
+app.use(cors(corsOptions));
+
+app.use("/api/auth", authRoutes);
+app.use("/api/users", userRoutes);
+app.use("/api/posts", postRoutes);
+app.use("/api/notifications", notificationRoutes);
+app.use("/api/search", searchRoutes);
 
 if (process.env.NODE_ENV === "production") {
-    app.use(express.static(path.join(__dirname, "/client/dist")));
+  app.use(express.static(path.join(__dirname, "/client/dist")));
 
-    app.get("*", (req, res) => {
-        res.sendFile(path.resolve(__dirname, "client", "dist", "index.html"));
-    });
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "dist", "index.html"));
+  });
 }
 
 app.listen(PORT, () => {
-    connectMongoDB()
-    console.log(`Server is running at port: ${PORT}`)
-})
+  connectMongoDB();
+  console.log(`Server is running at port: ${PORT}`);
+});
